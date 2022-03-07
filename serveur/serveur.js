ad32727f421f467fa3e4098a55a8b658ba81c1ff
Cédric <60275035+CedricLo@users.noreply.github.com>
2022-03-07 18:16:11 +0100

    ajout requestListener

--- serveur/serveur.js
+++ serveur/serveur.js
@@ -7,9 +7,19 @@ const port = 3030;
    
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');  
-  res.end('Hello World\n');  
+  res.end('Hello World !!\n');  
 });
    
 server.listen(port, hostname, () => {  
   console.log(`Server running at http://${hostname}:${port}/`);  
+  requestListener();
 });  
+
+const requestListener = function (req, res) {
+    switch(req.url){
+        case "/" : 
+            res.writeHead(200);
+            res.end("Default response");
+
+    }
+}
