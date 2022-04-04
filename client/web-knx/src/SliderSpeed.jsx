b197599f83ef674110459918a460f5d1d3c92173
pgastou <90198841+pgastou@users.noreply.github.com>
2022-04-04 10:23:50 +0200

    ajout du client

---
+++ client/web-knx/src/SliderSpeed.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Slider from '@mui/material/Slider'; 
+import {Stack,Divider, createTheme } from '@mui/material';
+
+
+
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#0971f1',
+      darker: '#053e85',
+    },
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+  },
+});
+
+export default class SliderSpeed extends React.Component {
+  vitesse = this.props.vitesse;
+  vitesseChange = this.props.vitesseChange;
+  state = {
+    button: {
+      nameButton: "Vitesse"
+    }
+  };
+
+  accelererChenillard() {
+    if (this.props.vitesse < 5) {
+      const vitesse = this.props.vitesse + 1;
+      this.vitesseChange(vitesse);
+    }
+    console.log(this.props.vitesse);
+  }
+
+  ralentirChenillard() {
+    if (this.props.vitesse > 0) {
+      const vitesse = this.props.vitesse - 1;
+      this.vitesseChange(vitesse);
+    }
+    console.log(this.props.vitesse);
+  }
+
+  onChange =( event)=>{
+    this.vitesseChange(event.target.value)
+  }
+
+  render() {
+    return (
+      <Stack 
+      spacing={2} direction="column" sx={{ mb: 1 }} alignItems="center">
+        
+          <h1>{this.state.button.nameButton}</h1>
+        
+          <Slider 
+            aria-label="Vitesse" 
+            value={this.props.vitesse} 
+            onChange={this.onChange} 
+            color = "secondary"
+            size = "medium"/>
+          
+      </Stack>
+    );
+  }
+}
---
+++ client/web-knx/src/requetes.js
@@ -0,0 +1,27 @@
+const url = "https://c1e4-148-60-140-218.ngrok.io/"
+const url2 = "http://127.0.0.1:3030"
+
+
+
+module.exports={
+    /**
+     * GET : 
+     * POST : 
+     * DELETE :
+     * PUT :
+     */
+
+    postChenillardState : function(etat,vitesse,sens, socket){        
+    },
+    
+
+}
+
+function chenillardState(etat,vitesse,sens){
+        return {
+            "chenillard" : "chenillard",
+            "etat" : etat, 
+            "vitesse" : vitesse, 
+            "sens" : sens
+        };
+}
