07700927dcac7bdf9cf160fdd6347feb6e2860f1
pgastou <90198841+pgastou@users.noreply.github.com>
2022-04-29 16:44:15 +0200

    début du knx

--- serveur/serveurExpress.js
+++ serveur/serveurExpress.js
@@ -5,54 +5,56 @@ var fs = require("fs");
 const { client } = require('websocket');
 // Importing the required modules
 const WebSocketServer = require('ws');
- //127.0.0.1
- const cors = require('cors')
+//127.0.0.1
+const cors = require('cors')
 
+const knxServer = require("./serveur-knx")
 
- /**
-  * Chenillard initialisation
-  */
- class Chenillard {
-    Chenillard(state,speed,sens){
+/**
+ * Chenillard initialisation
+ */
+class Chenillard {
+    Chenillard(state, speed, sens) {
         this.state = state;
         this.speed = speed;
         this.sens = sens;
     }
 
-    setState(newState){
+    setState(newState) {
         this.state = newState;
-        console.log('chenillard state : ' +this.state)
+        console.log('chenillard state : ' + this.state)
     }
 
-    setSpeed(newSpeed){
+    setSpeed(newSpeed) {
         this.speed = newSpeed;
+        knxServer.chenillardSpeed(this.speed);
     }
 
-    setSens(newSens){
+    setSens(newSens) {
         this.sens = newSens;
     }
 }
 
-var knxChenillard = new Chenillard(false,1,'gauche');
+var knxChenillard = new Chenillard(false, 1, 'gauche');
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
-app.post('/home', (req,res) => {
-    console.log('POST REQUETE /home : ',req);
-    console.log('DATA',req.body);
+app.post('/home', (req, res) => {
+    console.log('POST REQUETE /home : ', req);
+    console.log('DATA', req.body);
     res.send('Ok')
     broadcast(req.body)
 })
 
-app.post('/', (req,res) => {
-    console.log('POST REQUETE / : ',req);
-    console.log('DATA',req.body);
+app.post('/', (req, res) => {
+    console.log('POST REQUETE / : ', req);
+    console.log('DATA', req.body);
     res.send('200')
 })
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     console.log('GET REQUEST / : ', req);
 })
 
@@ -61,16 +63,16 @@ var server = app.listen(8080, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Listening at http://%s:%s", host, port)
- })
+})
 
- server.on('upgrade', function (_, socket){
-     console.log('upgrade event fired', socket);
- });
+server.on('upgrade', function (_, socket) {
+    console.log('upgrade event fired', socket);
+});
 
 // Creating a new websocket server
-const wss = new WebSocketServer.Server({ address : "https://dc30-2a02-8440-7210-39b8-83d-1264-bd9-2524.ngrok.io" ,port: 3030 });
+const wss = new WebSocketServer.Server({ address: "https://dc30-2a02-8440-7210-39b8-83d-1264-bd9-2524.ngrok.io", port: 3030 });
 
-function broadcast(data){
+function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(JSON.stringify(data));
     });
@@ -89,10 +91,10 @@ wss.getUniqueID = function () {
 wss.on("connection", ws => {
     console.log("new client connected");
     ws.id = wss.getUniqueID();
-/*
-    wss.clients.forEach(function each(client) {
-        console.log('Client.ID: ' + client.id);
-    });*/
+    /*
+        wss.clients.forEach(function each(client) {
+            console.log('Client.ID: ' + client.id);
+        });*/
 
 
     ws.on("message", data => {
@@ -102,13 +104,13 @@ wss.on("connection", ws => {
         });
 
         parsedData = JSON.parse(data);
-        if(parsedData.state != undefined) {
+        if (parsedData.state != undefined) {
             knxChenillard.setState(parsedData.state);
         }
-        else if(parsedData.speed != undefined) {
+        else if (parsedData.speed != undefined) {
             knxChenillard.setSpeed(parsedData.speed);
         }
-        else if(parsedData.sens != undefined) {
+        else if (parsedData.sens != undefined) {
             knxChenillard.setSens(parsedData.sens);
         }
         else {
