c8004e34043fab2152dc77bd9fa78e3d1a48d648
pgastou <90198841+pgastou@users.noreply.github.com>
2022-05-10 15:10:36 +0200

    avancée knx

--- serveur/testKnx.js
+++ serveur/testKnx.js
@@ -2,8 +2,15 @@ const knx = require('knx');
 const exitHook = require('async-exit-hook');
 const readline = require('readline');
 const { stdin: input, stdout: output, exit, stderr, mainModule } = require('process');
+const { set } = require('express/lib/application');
+const { jsonp } = require('express/lib/response');
 
 var wssLoc;
+var connection;
+var light1
+var light2
+var light3
+var light4
 
 module.exports = {
     serveurKnxInit: function (wss) {
@@ -64,10 +71,13 @@ function main(){
     
 const rl = readline.createInterface({ input, output });
    connectionKnx()
-   declarationLampes()
+   //declarationLampes()
     var intervalRoutineID;
     rl.on('line', (data)=>{
         switch(data){
+            case 'd' :
+                declarationLampes();
+            break ;
             case 'l1o' :
                 eteindreL1();
             break ;
@@ -121,51 +131,15 @@ const rl = readline.createInterface({ input, output });
 
 
 function connectionKnx() {
-
-    var connection = knx.Connection({
-        handlers: {
-            connected: function () {
-                console.log('----------');
-                console.log('Connected!');
-                console.log('----------');
-                var dp = new knx.Datapoint({ ga: '1/1/1' }, connection);
-                // Now send off a couple of requests:
-                dp.read((src, value) => {
-                    console.log("**** RESPONSE %j reports current value: %j", src, value);
-                });
-                dp.write(1);
-            }
-        }
-    });
-
-
-    // declare a simple binary control datapoint
-    var binary_control = new knx.Datapoint({ ga: '1/0/1', dpt: 'DPT1.001' });
-    // bind it to the active connection
-    binary_control.bind(connection);
-    // write a new value to the bus
-    binary_control.write(true); // or false!
-    // send a read request, and fire the callback upon response
-    binary_control.read(function (response) {
-        console.log("KNX response: %j", response);
-    });
-    // or declare a dimmer control
-    var dimmer_control = new knx.Datapoint({ ga: '1/2/33', dpt: 'DPT3.007' });
-    // declare a binary STATUS datapoint, which will automatically read off its value
-    var binary_status = new knx.Datapoint({ ga: '1/0/1', dpt: 'DPT1.001', autoread: true });
-
-
-    var connection = new knx.Connection({
+    connection = new knx.Connection({
         // ip address and port of the KNX router or interface
-        ipAddr: '192.168.0.201', ipPort: 3671,
-        // in case you need to specify the multicast interface (say if you have more than one)
-        interface: 'eth0',
+        ipAddr: '192.168.0.202', ipPort: 3671,
+        
         // the KNX physical address we'd like to use
         physAddr: '15.15.15',
         // set the log level for messsages printed on the console. This can be 'error', 'warn', 'info' (default), 'debug', or 'trace'.
         loglevel: 'info',
-        // do not automatically connect, but use connection.Connect() to establish connection
-        manualConnect: true,
+        
         // use tunneling with multicast (router) - this is NOT supported by all routers! See README-resilience.md
         forceTunneling: true,
         // wait at least 10 millisec between each datagram
@@ -180,22 +154,36 @@ function connectionKnx() {
             connected: function () {
                 console.log('KNX is connected');
                 // WRITE an arbitrary boolean request to a DPT1 group address
-                connection.write("0/0/1", 1);
-                connection.write("0/0/1", 0);
+                connection.write("0/0/1",1)
                 // you also WRITE to an explicit datapoint type, eg. DPT9.001 is temperature Celcius
                // connection.write("2/1/0", 22.5, "DPT9.001");
                 // you can also issue a READ request and pass a callback to capture the response
-               // connection.read("1/0/1", (src, responsevalue) => {
-                 //   console.log("src : ", src)
-                  //  console.log("response value : ", responsevalue)
-               // });
+                connection.read("1/0/1", (src, responsevalue) => {
+                    console.log("src : ", src)
+                    console.log("response value : ", responsevalue)
+                });
             },
             // get notified for all KNX events:
             event: function (evt, src, dest, value) {
+                valueParse = JSON.parse(JSON.stringify(value))
                 console.log(
                     "event: %s, src: %j, dest: %j, value: %j",
                     evt, src, dest, value
                 );
+
+                switch(dest){
+                    case "1/0/2" :
+                        if(valueParse.data[0] === 0){
+                            allumerL2()
+                        }
+                        else{
+                            eteindreL2()
+                        }
+                        break;
+                }
+
+
+
             },
             // get notified on connection errors
             error: function (connstatus) {
@@ -203,14 +191,32 @@ function connectionKnx() {
             }
         }
     });
+       // declare a simple binary control datapoint
+    var binary_control = new knx.Datapoint({ ga: '0/0/1', dpt: 'DPT1.001' });
+    // bind it to the active connection
+    binary_control.bind(connection);
+    // write a new value to the bus
+    binary_control.write(true); // or false!
+    // send a read request, and fire the callback upon response
+    binary_control.read(function (response) {
+        console.log("KNX response: %j", response);
+    });
+    // or declare a dimmer control
+    var dimmer_control = new knx.Datapoint({ ga: '1/2/33', dpt: 'DPT3.007' });
+    // declare a binary STATUS datapoint, which will automatically read off its value
+    var binary_status = new knx.Datapoint({ ga: '1/0/1', dpt: 'DPT1.001', autoread: true });
 
 
+    connection.Connect();
+    declarationLampes();
 
+    allumerL3();
+
+    deconnectionKnx();
     // Write raw buffer to a groupaddress with dpt 1 (e.g light on = value true = Buffer<01>) with a bitlength of 1
-    connection.writeRaw('1/0/0', Buffer.from('01', 'hex'), 1)
+   // connection.writeRaw('1/0/0', Buffer.from('01', 'hex'), 1)
     // Write raw buffer to a groupaddress with dpt 9 (e.g temperature 18.4 °C = Buffer<0730>) without bitlength
-    connection.writeRaw('1/0/0', Buffer.from('0730', 'hex'))
-
+    //connection.writeRaw('1/0/0', Buffer.from('0730', 'hex'))
 }
 
 /**
@@ -263,7 +269,7 @@ function chenillardStop(intervallID){
 }
 
 function allumerL1() {
-    light1.switchOn();
+    //light1.switchOn()
     console.log("lampe 1 allumée ");
 
 }
@@ -281,7 +287,7 @@ function allumerL4() {
 }
 
 function eteindreL1() {
-    //light1.switchOff();
+    //light1.switchOff()
     console.log("lampe 1 éteinte ");
 }
 function eteindreL2() {
@@ -298,7 +304,7 @@ function eteindreL4() {
 }
 
 function declarationLampes() {
-    var light1 = new knx.Devices.BinarySwitch({ ga: '0/0/1', status_ga: '0/1/1' }, connection);
+    light1 = new knx.Devices.BinarySwitch({ ga: '0/0/1', status_ga: '0/0/101' }, connection);
     console.log("The current light status is %j", light1.status.current_value);
     light1.control.on('change', function (oldvalue, newvalue) {
         console.log("**** LIGHT 1 control changed from: %j to: %j", oldvalue, newvalue);
@@ -307,7 +313,7 @@ function declarationLampes() {
         console.log("**** LIGHT 1 status changed from: %j to: %j", oldvalue, newvalue);
     });
 
-    var light2 = new knx.Devices.BinarySwitch({ ga: '0/0/2', status_ga: '0/1/2' }, connection);
+    light2 = new knx.Devices.BinarySwitch({ ga: '0/0/2', status_ga: '0/1/2' }, connection);
     console.log("The current light status is %j", light2.status.current_value);
     light2.control.on('change', function (oldvalue, newvalue) {
         console.log("**** LIGHT 2 control changed from: %j to: %j", oldvalue, newvalue);
@@ -316,7 +322,7 @@ function declarationLampes() {
         console.log("**** LIGHT 2 status changed from: %j to: %j", oldvalue, newvalue);
     });
 
-    var light3 = new knx.Devices.BinarySwitch({ ga: '0/0/3', status_ga: '0/1/3'}, connection);
+    light3 = new knx.Devices.BinarySwitch({ ga: '0/0/3', status_ga: '0/1/3'}, connection);
     console.log("The current light status is %j", light3.status.current_value);
     light3.control.on('change', function (oldvalue, newvalue) {
         console.log("**** LIGHT 3 control changed from: %j to: %j", oldvalue, newvalue);
@@ -325,7 +331,7 @@ function declarationLampes() {
         console.log("**** LIGHT 3 status changed from: %j to: %j", oldvalue, newvalue);
     });
 
-    var light4 = new knx.Devices.BinarySwitch({ ga: '0/0/4', status_ga: '0/1/4' }, connection);
+    light4 = new knx.Devices.BinarySwitch({ ga: '0/0/4', status_ga: '0/1/4' }, connection);
     console.log("The current light status is %j", light4.status.current_value);
     light4.control.on('change', function (oldvalue, newvalue) {
         console.log("**** LIGHT 4 control changed from: %j to: %j", oldvalue, newvalue);
@@ -333,6 +339,8 @@ function declarationLampes() {
     light4.status.on('change', function (oldvalue, newvalue) {
         console.log("**** LIGHT 4 status changed from: %j to: %j", oldvalue, newvalue);
     });
+
+    light3.switchOn()
 
 }
 
